e50d6cc1299a3f92ed2116310f82db9d66f44ee4
maksymilan <3510040005@qq.com>
2026-01-12 13:11:03 +0800

    links

---
+++ src/data/friend.ts
@@ -0,0 +1,21 @@
+export interface Friend {
+	name: string;
+	description: string;
+	avatar: string;
+	url: string;
+}
+
+export const friends: Friend[] = [
+	{
+		name: "HollowGL",
+		description: "HollowsGL's site",
+		avatar: "https://github.com/HollowGL.png",
+		url: "https://hollowgl.github.io/",
+	},
+	{
+		name: "NoughtQ",
+		description: "Be different, be the one!",
+		avatar: "https://github.com/NoughtQ.png",
+		url: "https://blog.noughtq.top/",
+	},
+];
